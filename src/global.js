8c89de9990d46856d40d9d9eeef24563bfb90410
vishal Coder <[vishal2app@gmail.com]>
2022-07-28 11:58:48 +0530

    bug fixex

--- src/global.js
+++ src/global.js
@@ -1,3 +1,2 @@
-//export const API = "https://62b19a7b196a9e9870390f35.mockapi.io";
-// export const API = "https://node-guvi.herokuapp.com";
-export const API = "http://localhost:3004";
+export const API = "https://node-guvi.herokuapp.com";
+//export const API = "http://localhost:3004";
